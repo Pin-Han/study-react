d1698ec680d76379c30258b43c2baa3d24aed768
Pin-Han <patrick04184@gmail.com>
2022-01-23 16:58:28 +0800

    practice useEffect

--- lesson-9/src/App.js
+++ lesson-9/src/App.js
@@ -6,6 +6,7 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     const userLoggedInfo = localStorage.getItem("isLoggedIn");
     if (userLoggedInfo === "1") {
